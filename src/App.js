ecdda03a262e6b8a107db77ac0b6626116e3932e
John Solo <johnsolojoseph@gmail.com>
2018-11-04 10:48:08 -0800

    Added AppBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import AppBar from '@material-ui/core/AppBar';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <AppBar position="static">
+        <Typography> Image Box
+        </Typography>
+      </AppBar>
       </div>
+
+
+
     );
   }
 }
