253b40dcb762573e21e6739765f551eaed8f423f
John Solo <johnsolojoseph@gmail.com>
2018-11-12 12:11:14 -0800

    feat: added slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Grid, Paper, MuiThemeProvider, createMuiTheme, Typography, withStyles} from '@material-ui/core';
+import PropTypes from 'prop-types';
+
 import NavBar from './components/NavBar.js'
-import AppBar from '@material-ui/core/AppBar';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import Slider from './components/Slider.js'
+
+
 
 
 import './App.css';
@@ -11,16 +14,28 @@ import './App.css';
 
 
 class App extends Component {
-  render() {
-    return (
+      render() {
+      return (
       <div>
       <NavBar />
+      <Grid container>
+      <Grid item xs={6}>
+      <Typography>Contrast</Typography>
+      <Slider />
+      <Typography>Brightness</Typography>
+      <Slider />
+      <Typography>Saturate</Typography>
+      <Slider />
+      <Typography>Sepia</Typography>
+      <Slider />
+      </Grid>
+      </Grid>
+
+
       </div>
-
-
-
     );
   }
+
 }
 
 export default App;
