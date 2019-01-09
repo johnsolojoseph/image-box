88a181f9bd183ec13753f67d7825059eddf1c2fb
John Solo <johnsolojoseph@gmail.com>
2019-01-09 14:52:22 -0800

    Restructured file format

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import NavBar from './NavBar';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <NavBar title="ImageBox" />
+      </div>
+
+
+    );
+  }
+
+}
+
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,17 @@
+import './NavBar.css';
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-};
+class NavBar extends React.Component {
+  render() {
+    return(
+      <nav>
+        <div className="nav-wrapper container">
+        <a href="#" className="brand-logo">{this.props.title}</a>
+        </div>
+      </nav>
 
-function NavBar(props) {
-  const { classes } = props;
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Image Box
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    );
+  }
 }
 
-NavBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(NavBar);
+export default NavBar;
